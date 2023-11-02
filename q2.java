1944db9ac5edbbb4d732b23a337e1b31d340afeb
Prajna Paramita Biswal <137276745+IAmPrajna@users.noreply.github.com>
2023-11-02 23:40:53 +0530

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class q2 {
+		   public static void insertionSort(int[] a){
+		        int n = a.length;
+		        for(int i = 1; i < n; i++){
+		            int j = i;
+		            while(j > 0 && a[j] < a[j-1]){
+		                int temp = a[j];
+		                a[j] = a[j-1];
+		                a[j-1] = temp;
+		                j--;
+		            }
+		        }
+		    }
+
+		    public static void main(String[] args) {
+		        int[] a = {8, 3, 6, 5, 4, 2,11,9};
+		        insertionSort(a);
+		        for(int val : a){
+		            System.out.print(val + " ");
+		        }
+		    }
+	
+	}
+
+
